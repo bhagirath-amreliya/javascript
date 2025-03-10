e8e03d2218744c946344c904bfaf56fbafe04f50
TeamF4L <ogitachi4@gmail.com>
2025-03-10 15:59:22 +0530

    if/else

---
+++ java-1/if/else.js
@@ -0,0 +1,40 @@
+
+
+// if/else =>(condditinal)
+
+let a =20 , b=40;
+
+if(a > b){
+    console.log("a greter then b");
+}
+else(a > b)
+{
+    console.log("b greter then a");
+}
+
+let anish = 18
+let zeel = 20
+
+if(anish <= zeel){
+    console.log("anish greter then zeel");
+}
+else(zeel >= anish)
+{
+    console.log("zeel greter then anish");
+}
+
+
+let age = 15
+
+if(age >= 18){
+    console.log("you allow for smoking");
+}
+else{
+    console.log("you are not allow to smoking");
+    
+}
+
+
+
+
+    
