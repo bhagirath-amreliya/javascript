ffff7ce5eeb275d225a1431a00e23b954b8fda80
TeamF4L <ogitachi4@gmail.com>
2025-03-18 14:41:12 +0530

    project-2

---
+++ java-pr-2/js-pr-2/pr-2.js
@@ -0,0 +1,164 @@
+
+
+// 1. Enter Electricity UNIT and Calculate amount to pay   
+
+// let unit= 340;
+// let price1;
+// let price2;
+// let price3;
+// let price4;
+// let pricepr;
+// let finalbill;
+// let finalbill2;
+
+
+// if (unit <= 50){
+//     price1=unit;
+//     console.log("your bill", price1 );
+// }else if(unit >= 50 &&  unit <= 150){
+//     price2=2*unit;
+//     console.log("your bill", price2);
+// }else if (unit >= 150 && unit <= 250){
+//   price3=3*unit;
+//   pricepr=price3*0.2;  
+//   finalbill=(price3)+(pricepr);
+//   console.log("your bill" , finalbill);
+// } else if (unit >=250){
+//     price4=4*unit;
+//     pricepr=price4*0.2;
+//     finalbill2=(price4)+(pricepr);
+//     console.log("your final bil", finalbill2);
+// }else{
+//     console.log("");
+    
+// }
+
+
+
+// 2. using switch case...  
+
+
+// let number = 2;
+
+// switch(number){
+//     case 1:
+//         console.log("it's winter");
+
+//         break;
+        
+//     case 2:
+//         console.log("it's winter");
+
+//         break;
+    
+//     case 3:
+//         console.log("it's winter");
+
+//         break;
+       
+//     case 4:
+//         console.log("it's winter");
+
+//         break;
+
+//     case 5:
+//         console.log("it's summer");
+
+//         break;
+
+//     case 6: 
+//         console.log("it's summer");
+
+//         break;
+
+//     case 7:
+//         console.log("it's summer");
+
+//         break;
+
+//     case 8:
+//         console.log("it's summer");
+            
+//         break;
+
+//     case 9:
+//         console.log("it's moonson");
+
+//         break;
+
+//     case 10:
+//         console.log("it's moonson");
+
+//         break;
+
+//     case 11:
+//         console.log("it's moonson");
+
+//         break;
+
+//     case 12:
+//         console.log("it's moonson");
+
+//         break;   
+// }
+
+
+
+// 3.  Find a rate of intrest  
+
+
+    // let p = 13000000;
+    // let r;
+    // let n = 3;
+    // let finalamount;
+
+    // if (n>=3&&n<=5){
+    //     r=3;
+    //     finalamount=(p*r*n)/100;
+    //     console.log("your final intrest is ", finalamount);
+    // }else if (n>=5 && n<=8){
+    //     r=5;
+    //     finalamount=(p*r*n)/100;
+    //     console.log("your final intrest is ", finalamount);
+    // }else if (n>=8 && n<=12){
+    //     r=12;
+    //     finalamount =(p*r*n)/100;
+    //     console.log("your final intrest is ", finalamount);
+    // }else if (n >= 12){
+    //     r = 15;
+    //     finalamount=(p*r*n)/100;
+    //     console.log("final intrest is ", finalamount);
+    // }else{
+    //     console.log();
+        
+    // }
+
+
+
+// 4 . age group 
+
+// let age = 19;
+
+// if (age >= 18){
+//     console.log("you are eligible for voting");
+// }
+// else{
+//     console.log("you are not eligible for voting");
+// }
+
+
+
+// 5 classify the age groups..  
+
+
+// let a=24;
+
+// if (a<=14){
+//     console.log("you are children");
+// }else if (a>=14 && a<= 25){
+//    console.log("you are young ");
+// }else if (a>=25 && a<=64){
+//     console.log("you are adults ");
+// }else if (a>=64){
+//     console.log("you are senior cityzen");    
+// }
