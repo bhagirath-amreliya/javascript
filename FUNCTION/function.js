8426a14b7124859b5f053d8138b4e98638c35844
TeamF4L <ogitachi4@gmail.com>
2025-03-27 15:57:14 +0530

    type of function

--- FUNCTION/function.js
+++ FUNCTION/function.js
@@ -3,88 +3,208 @@
 
     // 1. NANR ==>
 
-        function plus(){
-            let a = 10, b = 30;
-            totl = a + b ;
-            console.log("totle of value : ",totl);
-        }
+        // function plus(){
+        //     let a = 10, b = 30;
+        //     totl = a + b ;
+        //     console.log("totle of value : ",totl);
+        // }
         // plus()
 
 
 
     // 2. NAWR ==> 
 
-        function mini(){
-           let c = 98, d = 59;
-           let totle = c - d;
-           return totle;
-        }
-        // mini()
-        // console.log("value of : ", totle);
+    //     function mini(){
+    //        let c = 98, d = 59;
+    //        let totle = c - d;
+    //        return totle;
+    //     }
+    //     // mini()
+    //     // console.log("value of : ", totle);
 
 
-    // 3. TANR ==> 
+    // // 3. TANR ==> 
 
-        function multiplication(a , b ){
-            t = a * b;
-            console.log("multiplication value of :", t);
-        }
-        // multiplication(23 , 45);
+    //     function multiplication(a , b ){
+    //         t = a * b;
+    //         console.log("multiplication value of :", t);
+    //     }
+    //     // multiplication(23 , 45);
 
 
-    // 4. TATR ==> 
+    // // 4. TATR ==> 
 
-        function divi(a , b ){
-            tot = a / b ;
-            return tot;
-        }
+    //     function divi(a , b ){
+    //         tot = a / b ;
+    //         return tot;
+    //     }
         
-        // divi(90 , 42)
-        // console.log("Divison of this value :", tot);
+    //     // divi(90 , 42)
+    //     // console.log("Divison of this value :", tot);
 
 
         
-        function bign(a , b){
-            if(a > b){
-                console.log("if a is big number of b ");
-            }else{
-                console.log("if b is big number of a ");               
-            }
-        }
-        // bign(10 , 30);
+    //     function bign(a , b){
+    //         if(a > b){
+    //             console.log("if a is big number of b ");
+    //         }else{
+    //             console.log("if b is big number of a ");               
+    //         }
+    //     }
+    //     // bign(10 , 30);
 
 
 
 
-        function switchcase(){
+    //     function switchcase(){
            
-            let num = +prompt("Enter Choice");
-        switch (num) {
+    //         let num = +prompt("Enter Choice");
+    //     switch (num) {
 
-            case 1:
-                plus(); 
-            break;
+    //         case 1:
+    //             plus(); 
+    //         break;
         
-            case 2 :
-                console.log(mini());              
-            break;
+    //         case 2 :
+    //             console.log(mini());              
+    //         break;
 
-            case 3:
-                multiplication(23 , 45);
-            break ;  
+    //         case 3:
+    //             multiplication(23 , 45);
+    //         break ;  
             
-            case 4:
-                console.log(divi(90 , 42));
-            break ;   
-           default:
-                console.log("Wrong Choice");
-                break;
+    //         case 4:
+    //             console.log(divi(90 , 42));
+    //         break ;   
+    //        default:
+    //             console.log("Wrong Choice");
+    //             break;
                    
-        }
+    //     }
 
-    }
+    // }
 
-    switchcase();
+    // switchcase();
+
+
+    // ==> Recursion Function <==
+
+    // function zeel(anish) {
+        
+    //     if(anish < 10){
+    //         console.log("Inside value of anish :", anish);
+    //         zeel(anish++);
+    //     }
+    //     console.log("outside value of anish :");  
+    // }
+
+    // zeel(1)
+
+
+    // function zeel(anish) {
+    //     if (anish < 10) {
+    //         console.log( "inside value of anish :" ,anish);
+    //         zeel(++anish);
+    //     }
+    //     console.log("outside value of anish :" , anish);     
+    // }
+    // zeel(1);
     
+
+    // fibonacci series   ==>
+
+    // function fibo(n) {
+    //     if (n <= 1) {
+    //         return n;
+    //     }
+    //     return fibo(n - 1) + fibo(n - 2);
+    // }
+    // console.log(fibo(10));
+
+
+
+    // Factorial ==>
+
+
+        // function fact() {
+        //     let num = +prompt("Enter Choice");
+        // let a = 1;
+        // for (let i = 1; i <= num; i++) {
+        // //     a *= i;
+        //     console.log(a);
+        // }  
+        // }
+        // fact()
+
+
+
+
+
+        // variable as a function ==> (It's a annoynimos function , no name is function)
+
+            // let a = function(){
+            //     console.log("This is variable as a function");
+            // }
+            // a()
+
+
+
+            // Arrow function ==> (It's also annoynimos function)
+
+                // let a = () => {
+                //     console.log("This is arrow function");
+                // }
+                // a()
+
+
+                // 2. 
+
+                // let b = (a) => a ** 2;
+                      
+                // console.log(b(5));
+
+
+
+                // IIFE function ==> (Immediately invoked function expression)
+
+                    // (function(){
+                    //     console.log("This is IIFE function");
+                    // })()
+
+
+
+
+                    // Nested function ==> 
+
+                        // function outer(a , b) {
+                        //     console.log("This is outer function" ,a * b);
+                        //     function inner(c , d) {
+                        //         console.log("This is inner function", c *= d);
+                        //     }
+                        //     return inner;
+                        // }
+                        // outer(20 , 20)(50 , 70)
+
+                        
+
+                    //  function fibo(n) {
+                    //     let a = 0 , b = 1;
+                    //     for (let i = 1; i <= n; i++) {
+                    //         console.log(a);
+                    //         let c = a + b;
+                    //         a = b;
+                    //         b = c;
+                    //     }
+                    //     return a;
+                    //  }    
+                    //  console.log(fibo(20));
+
+
+
+                    
+
+           
+
+
         
         
