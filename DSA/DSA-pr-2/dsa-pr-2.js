41ca0d7bb4e554a4b43bdb10e619d39f0b05c3f7
TeamF4L <ogitachi4@gmail.com>
2025-06-09 14:50:58 +0530

    dsa-pr-2

--- DSA/DSA-pr-2/dsa-pr-2.js
+++ DSA/DSA-pr-2/dsa-pr-2.js
@@ -173,74 +173,79 @@
 
 // 3.Write a Program for Singly Link List (insert, delete and display function)..
 
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null; 
-    }
-}
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.next = null; 
+//     }
+// }
 
-class LinkList {
-    constructor() {
-        this.head = null;
-    }
+// class LinkList {
+//     constructor() {
+//         this.head = null;
+//     }
 
-    lastInsert(data)
-    {
-        let newNode = new Node(data);
-        if(this.head == null){
-            this.head = newNode;
-            return;
-        }
-        else{
-            let current = this.head;
-            while(current.next != null){
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-    deleteNode(data) {
-        if (this.head == null) {
-            console.log("List is Empty !!");
-            return;
-        }
-        if (this.head.data === data) {
-            this.head = this.head.next;
-            return;
-        }
-        let current = this.head;
-        while (current.next != null && current.next.data !== data) {
-            current = current.next;
-        }
-        if (current.next == null) {
-            console.log("Node not found !!");
-        } else {
-            current.next = current.next.next;
-        }
-    }
+//     lastInsert(data)
+//     {
+//         let newNode = new Node(data);
+//         if(this.head == null){
+//             this.head = newNode;
+//             return;
+//         }
+//         else{
+//             let current = this.head;
+//             while(current.next != null){
+//                 current = current.next;
+//             }
+//             current.next = newNode;
+//         }
+//     }
+//     deleteNode(data) {
+//         if (this.head == null) {
+//             console.log("List is Empty !!");
+//             return;
+//         }
+//         if (this.head.data === data) {
+//             this.head = this.head.next;
+//             return;
+//         }
+//         let current = this.head;
+//         while (current.next != null && current.next.data !== data) {
+//             current = current.next;
+//         }
+//         if (current.next == null) {
+//             console.log("Node not found !!");
+//         } else {
+//             current.next = current.next.next;
+//         }
+//     }
 
-    display(){
-        if(this.head == null){
-            console.log("List is Empty !!");
-        }else{
-            let result = [];
-            let current = this.head;
-            while(current.next != null){
-                result.push(current.data);
-                current = current.next;
-            }
-            result.push(current.data);
-            console.log(result.join(" -> "));
-        }
-    }
-}
-let test = new LinkList();
-test.lastInsert(100);
-test.lastInsert(200);
-test.lastInsert(300);
-test.lastInsert(400);
-test.lastInsert(500);
-test.display();
-test.deleteNode(300);
-test.display();
+//     display(){
+//         if(this.head == null){
+//             console.log("List is Empty !!");
+//         }else{
+//             let result = [];
+//             let current = this.head;
+//             while(current.next != null){
+//                 result.push(current.data);
+//                 current = current.next;
+//             }
+//             result.push(current.data);
+//             console.log(result.join(" -> "));
+//         }
+//     }
+// }
+// let test = new LinkList();
+// test.lastInsert(100);
+// test.lastInsert(200);
+// test.lastInsert(300);
+// test.lastInsert(400);
+// test.lastInsert(500);
+// test.display();
+// test.deleteNode(300);
+// test.display();
+
+
+// Output:
+// 100 -> 200 -> 300 -> 400 -> 500
+// 100 -> 200 -> 400 -> 500
